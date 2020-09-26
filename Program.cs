8b23895d5540be096996cc68cb538778c5714c32
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-26 01:30:56 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+           double milhas;
+           double kms = 1.609;
+
+            Console.Write("digite uma medida em milha(s): ");
+            milhas = double.Parse(Console.ReadLine());
+
+            double resultado = milhas*kms;
+
+            Console.WriteLine($"o valor de milhas para quilometros É de..: {resultado}");
+            Console.WriteLine();
+            Console.WriteLine("Precione qualquer tecla para finalizar...");
+            Console.ReadKey();
+            Console.Clear(); 
+        }
+    }
+}
